224b403f6d39adaab9318ed1739245347f2f091c
Giancarlo Sanchez <g.sanchez@glofox.com>
2016-04-07 23:00:11 +0100

    Sitemap

--- server.js
+++ server.js
@@ -90,6 +90,12 @@ app.post('/add', function(req, res) {
   	});
 });
 
+/**************************************** SITEMAP AND ROBOTS ***********************************************/
+
+app.get('/robots.txt', function (req, res) { res.type('text/plain'); res.send("User-agent: *\nDisallow: /\nSitemap: http://katori-shinto-ryu.ie/sitemap.xml"); });
+app.get('/sitemap.xml', function(req, res) { res.header('Content-Type', 'text/xml'); res.send('<?xml version="1.0" encoding="UTF-8"?> <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"> <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com --><url><loc>http://katori-shinto-ryu.ie/</loc><changefreq>always</changefreq></url></urlset>'); });
+
+
 // app.use(function(req, res, next) {
 //   var contentType = req.headers['content-type'] || ''
 //     , mime = contentType.split(';')[0];
